809ffd6e49a8ad6a5b784f4c4ea29d09b422f6dd
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2024-08-26 16:10:52 +0530

    Design Category Add Page

--- frontend/src/components/CategoryForm.jsx
+++ frontend/src/components/CategoryForm.jsx
@@ -7,24 +7,24 @@ const CategoryForm = ({
 }) => {
   return (
     <div className="p-3">
-      <form onSubmit={handleSubmit} className="space-y-3">
+      <form onSubmit={handleSubmit} className="space-y-3 flex flex-col justify-center items-center">
         <input
           type="text"
-          className="py-3 px-4 border rounded-lg w-full"
+          className="p-2  my-3 w-[30rem] border rounded-md bg-[#101011] text-white w-full"
           placeholder="Write category name"
           value={value}
           onChange={(e) => setValue(e.target.value)}
         />
 
         <div className="flex justify-between">
-          <button className="bg-pink-500 text-white py-2 px-4 rounded-lg hover:bg-pink-600 focus:outline-none focus:ring-2 foucs:ring-pink-500 focus:ring-opacity-50">
+          <button className="bg-pink-600 text-white py-2 px-14 mx-3 rounded-md hover:bg-pink-700 focus:outline-none focus:ring-2 foucs:ring-pink-500 focus:ring-opacity-50">
             {buttonText}
           </button>
 
           {handleDelete && (
             <button
               onClick={handleDelete}
-              className="bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-600 focus:outline-none focus:ring-2 foucs:ring-red-500 focus:ring-opacity-50"
+              className="bg-red-500 text-white py-2 px-14 rounded-lg hover:bg-red-600 focus:outline-none focus:ring-2 foucs:ring-red-500 focus:ring-opacity-50"
             >
               Delete
             </button>
--- frontend/src/pages/Admin/CategoryList.jsx
+++ frontend/src/pages/Admin/CategoryList.jsx
@@ -91,10 +91,10 @@ const CategoryList = () => {
   };
 
   return (
-    <div className="ml-[10rem] flex flex-col md:flex-row">
+    <div className="flex justify-center  pl-[4rem] ">
       <AdminMenu />
-      <div className="md:w-3/4 p-3">
-        <div className="h-12">Manage Categories</div>
+      <div className="bg-slate-800 rounded-md mt-40 p-3 ">
+        <div className="text-4xl text-center">Manage Categories</div>
         <CategoryForm
           value={name}
           setValue={setName}
@@ -103,11 +103,11 @@ const CategoryList = () => {
         <br />
         <hr />
 
-        <div className="flex flex-wrap">
+        <div className="flex flex-wrap ">
           {categories?.map((category) => (
             <div key={category._id}>
               <button
-                className="bg-white border border-pink-500 text-pink-500 py-2 px-4 rounded-lg m-3 hover:bg-pink-500 hover:text-white focus:outline-none foucs:ring-2 focus:ring-pink-500 focus:ring-opacity-50"
+                className="bg-slate-900 border border-white text-white py-2 px-4 rounded-lg m-3 hover:bg-pink-600 hover:border-pink-800 transition-colors hover:text-white focus:outline-none foucs:ring-2 focus:ring-pink-500 focus:ring-opacity-50"
                 onClick={() => {
                   {
                     setModalVisible(true);
